de2df04577d3769a5530407c387734025adb5ae8
Miste <misterchangray@hotmail.com>
2018-04-29 19:03:39 +0800

    登陆日志表设计 常量统一获取

--- src/main/java/com/github/misterchangray/controller/ConstantController.java
+++ src/main/java/com/github/misterchangray/controller/ConstantController.java
@@ -1,12 +1,20 @@
 package com.github.misterchangray.controller;
 
 import com.github.misterchangray.common.NormalResponse;
+import com.github.misterchangray.common.enums.DBEnum;
+import com.github.misterchangray.dao.entity.Constant;
+import com.github.misterchangray.dao.entity.ConstantQuery;
+import com.github.misterchangray.dao.mapper.ConstantMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * 页面常量控制类
@@ -18,13 +26,29 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/v1/constant")
 public class ConstantController {
+    @Autowired
+    private ConstantMapper constantMapper;
+
 
     @ApiOperation(value = "获取所有常量枚举", notes = "返回所有页面常量枚举，前端做缓存")
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
-    public NormalResponse constant() {
+    public NormalResponse constant(@RequestParam(required = false) Integer pid, @RequestParam(required = false) String shortcut) {
         NormalResponse res = new NormalResponse();
+
+        ConstantQuery constantQuery = new ConstantQuery();
+        ConstantQuery.Criteria criteria = constantQuery.createCriteria();
+        criteria.andEnabledEqualTo(DBEnum.TRUE.getCode());
+        criteria.andDeletedEqualTo(DBEnum.FALSE.getCode());
+
+        if(null == pid) criteria.andIdEqualTo(pid);
+        if(null == shortcut) criteria.andShortcutEqualTo(shortcut);
+
+        List<Constant> constantList = constantMapper.selectByQuery(constantQuery);
 
         return res;
     }
+
+
+
 }
