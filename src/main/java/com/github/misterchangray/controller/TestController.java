bd24561c5f999650aa17c81f05592a01f0d15737
rui.zhang <zhangrui@ejie365.com>
2018-05-25 17:08:31 +0800

    整合redis到项目

---
+++ src/main/java/com/github/misterchangray/controller/TestController.java
@@ -0,0 +1,33 @@
+package com.github.misterchangray.controller;
+
+import com.github.misterchangray.common.NormalResponse;
+import com.github.misterchangray.service.common.CacheService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Created by rui.zhang on 2018/5/25.
+ * @author rui.zhang
+ * @version ver1.0
+ * @email misterchangray@hotmail.com
+ * @description
+ */
+@Controller
+@RequestMapping("/v1/test")
+public class TestController {
+    @Autowired
+    CacheService cacheService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public NormalResponse constant(@RequestParam(required = false) Integer pid) {
+        NormalResponse res = NormalResponse.newInstance();
+
+        cacheService.set("asdf", "123123123123");
+        return res;
+    }
+}
