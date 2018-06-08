1a7bbf77b1bb96f63a94b473966b1eb876a4468a
rui.zhang <zhangrui@ejie365.com>
2018-06-08 15:26:02 +0800

    权限验证方法抽取

--- src/main/java/com/github/misterchangray/common/interceptor/AuthInterceptor.java
+++ src/main/java/com/github/misterchangray/common/interceptor/AuthInterceptor.java
@@ -1,11 +1,9 @@
 package com.github.misterchangray.common.interceptor;
 
 
-import com.github.misterchangray.common.ResultSet;
 import com.github.misterchangray.common.annotation.Authentication;
 import com.github.misterchangray.common.enums.ResultEnum;
 import com.github.misterchangray.common.exception.ServiceException;
-import com.github.misterchangray.common.utils.JSONUtils;
 import com.github.misterchangray.service.user.UserService;
 import com.github.misterchangray.service.user.bo.UserSessionBo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +19,7 @@ import java.lang.reflect.Method;
  * 1.首先判断访问的方法是否包含 @Authentication 注解;
  * 2.再判断当前登录用户集合中是否存在该session；并校验session正确性
  *
- * tips:如果使用单点登录;则此处直接实现单点登录验证的逻辑;
+ * tips:请在 tokenValidate 方法中实现你自己的登录验证;
  *
  *
  * @author Rui.Zhang/misterchangray@hotmail.com
@@ -32,6 +30,16 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
     UserService userService;
     @Autowired
     UserSessionBo userSessionBo;
+
+    /**
+     * 在这个方法里实现你的权限验证逻辑
+     * 成功返回true;失败返回false
+     * @return
+     */
+    private boolean tokenValidate(String token) {
+        return userSessionBo.exist(token);
+    }
+
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -53,12 +61,12 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
         Authentication methodAnnotation = method.getAnnotation(Authentication.class);
         // 有 @Authentication 注解，需要认证
         if (null != classAnnotation || methodAnnotation != null) {
-            // 执行认证
+            // 执行权限认证
             String token = request.getHeader("Authentication");  // 从 http 请求头中取出 Authentication
 
             if (null == token) throw new ServiceException(ResultEnum.NEED_AUTH, "无token，请先登录");
 
-            if(false == userSessionBo.exist(token)) throw new ServiceException(ResultEnum.NEED_AUTH, "token异常，请重新登录");
+            if(tokenValidate(token)) throw new ServiceException(ResultEnum.NEED_AUTH, "token异常，请重新登录");
             return true;
         }
         return true;
